d8cad3551cfd47dabecf7b9c3743e647f2b44c36
migueldossantos011@gmail.com <migueldossantos011@gmail.com>
2020-06-02 14:40:52 -0300

    adicionando projeto em python para comparar numeros sorteados pela maquina e numeros digitados pelo usuario

---
+++ dado.py
@@ -0,0 +1,25 @@
+from random import randint
+
+while True:
+    try:
+        num = int(input("Digite o número que você acha que vai ser sorteado [0 para sair]: "))
+        dado = randint(1, 6)
+
+        if num == dado:
+            print(f'\n\nVocê acertou! jogador: {num} ---- computador: {dado} \n\n')
+
+        elif num != dado and num > 0 and num <= 6:
+            print(f'\n\nVocê errou! jogador: {num} ---- computador: {dado} \n\n')
+        
+        elif num > 6:
+            print('\n\nErro! Digite números de 1 á 6\n\n')
+
+        elif num == 0:
+            break
+        
+    except KeyboardInterrupt:
+        print('você apertou CTRL + C bye!')
+        break
+
+    except:
+        print('Erro interno')
